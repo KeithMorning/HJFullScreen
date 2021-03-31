6bca0e62ba964da56e6c98dc8c46ac387fdd7342
KeithXi <xiaofeng.xi@net263.com>
2021-03-31 19:41:00 +0800

    support full screen

---
+++ Example/HJFullScreen/CustomFullViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CustomFullViewController.swift
+//  HJFullScreen_Example
+//
+//  Created by KeithXi on 2021/3/31.
+//  Copyright © 2021 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class CustomFullViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
